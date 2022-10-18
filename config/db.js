f7786b0b7ae8bf1c9ec9094d4f560a78638c7ad9
sebastianto <sebastt.93@gmail.com>
2022-10-19 01:39:19 +1100

    db config fixed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     const connection = await mongoose.connect(
-      // "mongodb+srv://admin:xkj9bjf9HFW.fdu_evp@cluster0.lguxo.mongodb.net/TEST2?retryWrites=true&w=majority"
       process.env.MONGO_URI
       ,
       { useNewUrlParser: true, useUnifiedTopology: true }
